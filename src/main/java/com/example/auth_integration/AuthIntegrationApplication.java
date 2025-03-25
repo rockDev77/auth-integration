b1d1afaede72abc4bc5c30f6ff3f6fad3cafd257
HeungSeok Lee <gudtjrdl852@gmail.com>
2025-03-25 15:31:59 +0900

    Initial commit

---
+++ src/main/java/com/example/auth_integration/AuthIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthIntegrationApplication.class, args);
+	}
+
+}
